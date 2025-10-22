4fc48ad71170145ba158e172b485bea42ec36bf7
aakash1612 <varshneyakash46@gmail.com>
2025-10-22 16:30:00 +0530

    deploy-ready

--- server/index.js
+++ server/index.js
@@ -24,10 +24,7 @@ app.use(cors({
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         console.log('✅ MongoDB Connected');
     })
