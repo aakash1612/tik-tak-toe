d797e3784b6045acc47b6fbea165f43bb2b6806f
aakash1612 <varshneyakash46@gmail.com>
2025-10-30 01:37:33 +0530

    add chnaged nodemailer

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -1,8 +1,8 @@
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_SERVICE_HOST,
-  port: parseInt(process.env.EMAIL_SERVICE_PORT),
-  secure: process.env.EMAIL_SERVICE_PORT == 465, // Use 'true' if port is 465 (SMTPS), 'false' if 587 (STARTTLS)
+  port: parseInt(process.env.EMAIL_SERVICE_PORT)||587,
+  secure: false, // Use 'true' if port is 465 (SMTPS), 'false' if 587 (STARTTLS)
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS, 
@@ -13,13 +13,13 @@ const sendEmail = async (to, subject, htmlContent) => {
   try {
     const mailOptions = {
       from: process.env.EMAIL_USER,
-      to: to,
+      to,
       subject: subject,
       html: htmlContent,
     };
 
     let info = await transporter.sendMail(mailOptions);
-    console.log('✅ Email sent: %s', info.messageId);
+    console.log('✅ Email sent successfully:', info.messageId);
     return { success: true, messageId: info.messageId };
   } 
   catch (error) {
