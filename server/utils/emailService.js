0bc9a0cfcbb061d9e02e57f1371ad47f10f549bc
aakash1612 <varshneyakash46@gmail.com>
2025-11-04 23:05:11 +0530

    soem change

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -1,11 +1,9 @@
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_SERVICE_HOST,
-  port: parseInt(process.env.EMAIL_SERVICE_PORT)||587,
-  secure: false, // Use 'true' if port is 465 (SMTPS), 'false' if 587 (STARTTLS)
+  service: "gmail", // ✅ use Gmail preset instead of manual host/port
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, 
+    pass: process.env.EMAIL_PASS,
   },
 });
 
