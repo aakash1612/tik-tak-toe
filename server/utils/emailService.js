26ef714393e5626cbe30d454447f9c96940ac876
aakash1612 <varshneyakash46@gmail.com>
2025-11-04 21:39:31 +0530

    emailService update

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -1,12 +1,10 @@
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_SERVICE_HOST,
-  port: parseInt(process.env.EMAIL_SERVICE_PORT)||587,
-  secure: false, // Use 'true' if port is 465 (SMTPS), 'false' if 587 (STARTTLS)
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, 
-  },
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER, 
+        pass: process.env.EMAIL_PASS
+    }
 });
 
 const sendEmail = async (to, subject, htmlContent) => {
